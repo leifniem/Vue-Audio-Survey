a14cc01724edb8429d4f12f8b303c3664b68c09c
Leif Niemczik <leif.niem@gmail.com>
2020-07-12 14:14:26 +0200

    switching to recorder mixin structure

--- packages/AudioDataCollector/src/components/Question.vue
+++ packages/AudioDataCollector/src/components/Question.vue
@@ -3,10 +3,12 @@
 		<h3>
 			This is Question {{ parseInt(index) + 1 }} of {{ questions.length }}
 		</h3>
-		<p>
-			{{ this.questions[index].text }}
-		</p>
-		<Recorder :id="index" class="recorder" />
+		<div class="task">
+			<p>
+				{{ this.questions[index].text }}
+			</p>
+			<Recorder :id="index" class="recorder" />
+		</div>
 		<router-link :to="nextPage">
 			<button>
 				{{ buttonText }}
@@ -34,19 +36,20 @@ export default {
 				? '/question/' + (parseInt(this.index) + 1).toString()
 				: '/thanks'
 		},
-		buttonText () {
+		buttonText() {
 			return this.index < questions.length - 1
-			? 'Skip Question'
-			: 'Complete Questionnaire'
-		}
+				? 'Skip Question'
+				: 'Complete Questionnaire'
+		},
 	},
 }
 </script>
 
 <style>
-	.recorder{
-		max-width: 100%;
-		box-shadow: none;
-		padding: 2rem;
-	}
+.task {
+	background: var(--light-background);
+	padding: 1rem 2rem 2rem;
+	margin-bottom: 2rem;
+	border-radius: 5px;
+}
 </style>
