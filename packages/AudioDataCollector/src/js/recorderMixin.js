48c3b9cc767d02abf9083cef19ef7ebc9ddb08d3
Leif Niemczik <leif.niem@gmail.com>
2020-07-13 01:30:40 +0200

    encountered 'this.requestData() is not a function' error with polyfill

--- packages/AudioDataCollector/src/js/recorderMixin.js
+++ packages/AudioDataCollector/src/js/recorderMixin.js
@@ -1,58 +1,81 @@
+import { recorderOptions, maxTakeLength } from '@/config'
+
 export default {
-	data () {
+	data() {
 		return {
 			hasPermission: false,
 			isRecording: false,
 			chunks: [],
-			currentBlob: null
+			currentBlob: null,
+			currentURL: null,
+			timer: null,
 		}
 	},
 
 	methods: {
-		async getStream () {
+		async getStream() {
 			const stream = await navigator.mediaDevices.getUserMedia({
 				audio: true,
 				video: false,
 			})
 			return stream
 		},
-		createRecorder () {
-			if(!this.$_stream) return
+		createRecorder() {
+			if (!this.$_stream) return
+			this.$_recorder = new MediaRecorder(this.$_stream, recorderOptions)
 			this.$_recorder.ignoreMutedMedia = true
 			this.$_recorder.addEventListener('start', () => {
 				this.isRecording = true
+				if (maxTakeLength && maxTakeLength > 0) {
+					this.timer = setTimeout(this.$_recorder.stop, maxTakeLength)
+				}
+				console.log('Started recording')
 			})
-			this.$_recorder.addEventListener('dataavailable', (e) => {
-				if (e.data && e.data.size > 0) this.chunks.push(e.data)
-			}, true)
-			this.$_recorder.addEventListener('stop', () => {
-				this.isRecording = false
-				this.currentBlob = new Blob(this.chunks)
-				this,chunks = []
-			}, true)
+			this.$_recorder.addEventListener(
+				'dataavailable',
+				(e) => {
+					if (e.data && e.data.size > 0) this.chunks.push(e.data)
+				},
+				true
+			)
+			this.$_recorder.addEventListener(
+				'stop',
+				() => {
+					console.log('Stopped recording')
+					if (this.timer) clearTimeout(this.timer)
+					this.timer = null
+					this.isRecording = false
+					this.currentBlob = new Blob(this.chunks)
+					this.currentURL = URL.createObjectURL(this.currentBlob)
+					this.chunks = []
+				},
+				true
+			)
 		},
-		async startRecording () {
+		async startRecording() {
 			if (this.isRecording) return
 			try {
-				if (this.$_recorder = null) {
-					this.$_stream = await getStream()
+				if (this.$_recorder == null) {
+					this.$_stream = await this.getStream()
 					this.createRecorder()
+					this.$_recorder.start()
+				} else {
+					this.$_recorder.start()
 				}
-				this.$_recorder.start()
 			} catch (err) {
-				console.error(err);
+				console.error(err)
 			}
 		},
-		stopRecording () {
+		stopRecording() {
 			if (!this.isRecording) return
 			this.$_recorder.stop()
-			this.$_stream.getTracks().forEach(track => track.stop())
+			this.$_stream.getTracks().forEach((track) => track.stop())
 		},
 	},
 
-	mounted () {
+	mounted() {
 		if (MediaRecorder.notSupported) {
 			console.error('MediaRecorder API is not supported')
 		}
-	}
+	},
 }
