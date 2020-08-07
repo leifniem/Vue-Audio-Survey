35e48b2121703641d0d5dd063f03538550166b8a
Leif Niemczik <leif.niem@gmail.com>
2020-08-07 19:32:04 +0200

    added support for select and radio inputs as metadata form fields

--- packages/frontend/src/components/Metadata.vue
+++ packages/frontend/src/components/Metadata.vue
@@ -6,11 +6,52 @@
 				<label :for="meta.id">{{
 					meta.name + (meta.required ? '*' : '')
 				}}</label>
-				<input :type="meta.type" :id="meta.id" :ref="meta.id" />
+				<span class="description" v-if="meta.description">{{
+					meta.description
+				}}</span>
+
+				<!-- Create a select with given options if type is 'select' -->
+				<select
+					:name="meta.name"
+					:id="meta.id"
+					v-if="meta.type === 'select'"
+					v-model="viewModelValues[meta.id]"
+				>
+					<option
+						v-for="option in meta.options"
+						:key="option"
+						:value="option"
+					>
+						{{ option }}
+					</option>
+				</select>
+
+				<!-- Create a fieldset for radio buttons with the according v-model if metadata type is 'radio' -->
+				<fieldset
+					:name="meta.name"
+					:id="meta.id"
+					v-else-if="meta.type === 'radio'"
+				>
+					<div v-for="option in meta.options" :key="option">
+						<input type="radio" :value="option" :id="option" v-model="viewModelValues[meta.id]" :name="meta.id"/>
+						<label :for="option">
+							{{ option }}
+						</label>
+					</div>
+				</fieldset>
+
+				<!-- V-Else for all inputs that only support single input -->
+				<input
+					:type="meta.type"
+					:id="meta.id"
+					v-model="viewModelValues[meta.id]"
+					v-else
+				/>
+
 				<span class="error">{{ meta.error }}</span>
 			</div>
 			<p class="footnote">Fields annotated with a * are non-optional.</p>
-			<input type="submit" content="Submit form" hidden>
+			<input type="submit" content="Submit form" hidden />
 		</form>
 		<button @click="metaSubmit" type="submit">
 			Submit
@@ -21,9 +62,11 @@
 <script>
 import { metaData } from '@/config'
 
+let viewModelValues = {}
 // initialize the error field for displaying errors
 for (let metaKey in metaData) {
 	metaData[metaKey].error = ''
+	viewModelValues[metaData[metaKey].id] = ""
 }
 
 export default {
@@ -31,6 +74,7 @@ export default {
 	data() {
 		return {
 			metaData,
+			viewModelValues,
 		}
 	},
 	methods: {
@@ -40,7 +84,10 @@ export default {
 			// Validate meta field with its validator function and add error if given
 			for (let meta of this.metaData) {
 				meta.error = ''
-				if (!meta.validator(this.$refs[meta.id.toString()][0].value)) {
+				if (
+					meta.hasOwnProperty('validator') &&
+					!meta.validator(this.viewModelValues[meta.id])
+				) {
 					errors = true
 					meta.error = meta.errorMessage
 				}
@@ -50,7 +97,7 @@ export default {
 			if (!errors) {
 				let finalMeta = {}
 				for (let meta of this.metaData) {
-					finalMeta[meta.id] = this.$refs[meta.id.toString()][0].value
+					finalMeta[meta.id] = this.viewModelValues[meta.id]
 				}
 				try {
 					this.$store.dispatch('writeMetaData', finalMeta)
@@ -75,7 +122,8 @@ label {
 	margin-bottom: 1rem;
 }
 
-input {
+input,
+select {
 	border: 3px solid $primary-light;
 	padding: 0.5rem;
 	border-radius: 5px;
@@ -87,6 +135,22 @@ input {
 	}
 }
 
+input[type='radio'] {
+	display: inline;
+	width: initial;
+}
+
+fieldset {
+	border: none;
+	padding: 0.5rem 0.25rem;
+	border-left: 3px solid $primary-light;
+
+	label {
+		display: inline;
+	}
+}
+
+.description,
 .footnote,
 .error {
 	font-size: 0.8rem;
